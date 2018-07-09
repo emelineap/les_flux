97c137fbaf820f2c6ef46529b92344deabaf2714
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2018-07-09 21:10:07 +0200

    homepage: last posts displayed, css fixed for the newsletter block, last videos and next workshops displayed, css on footer

--- wordpress/wp-content/themes/lesflux/front-page.php
+++ wordpress/wp-content/themes/lesflux/front-page.php
@@ -18,10 +18,11 @@ include 'header.php';
 		</div>
 	</div>
 
-	<div class="row">
+	<h2>DERNIERS ARTICLES</h2>
+
+	<div class="row row-last-posts">
 		<div class="last-posts">
 
-			<h2>Derniers articles</h2>
 
 			<!-- Get last 3 posts -->
 			<?php
@@ -42,7 +43,7 @@ include 'header.php';
 			) );
 
 			while (have_posts()) : the_post(); ?>
-			<ul>
+			<ul class="post-thumbnail-parent col-xs-12 col-lg-3">
 				<li><img class="post-thumbnail" src="<?php the_post_thumbnail(); ?></li>
 				<li><a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a></li>
 				<li><?php the_excerpt(__('(more…)')); ?></li>
@@ -50,53 +51,60 @@ include 'header.php';
 			<?php 
 		endwhile; 
 		?>
-
 	</div>
-</div>
 
-<div class="row">
-	<div class="newsletter">
-		<!-- Begin MailChimp Signup Form -->
-		<div id="mc_embed_signup"><form id="mc-embedded-subscribe-form" class="validate" action="https://tumblr.us14.list-manage.com/subscribe/post?u=5bd0d33a577ee592df934d133&amp;id=6ea9ff353e" method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank">
-			<div id="mc_embed_signup_scroll">
-				<p class="signup-newsletter">Abonnez-vous à la Newsletter de ma chatte</p>
-				<div class="mc-field-group"><label for="mce-EMAIL">
-				</label>
-				<div class="signup-block">
-					<div class="newsletter-input">
-						<input id="mce-EMAIL" class="required email" name="EMAIL" type="email" value="" placeholder="Votre email" />
-						<div id="mce-responses" class="clear">
-							<div id="mce-error-response" class="response" style="display: none;"></div>
-							<div id="mce-success-response" class="response" style="display: none;"></div>
+	<div class="row">
+		<div class="newsletter">
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup"><form id="mc-embedded-subscribe-form" class="validate" action="https://tumblr.us14.list-manage.com/subscribe/post?u=5bd0d33a577ee592df934d133&amp;id=6ea9ff353e" method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank">
+				<div id="mc_embed_signup_scroll">
+					<p class="signup-newsletter">Abonnez-vous à la Newsletter de ma chatte</p>
+					<div class="mc-field-group"><label for="mce-EMAIL">
+					</label>
+					<div class="signup-block">
+						<div class="newsletter-input">
+							<input id="mce-EMAIL" class="required email" name="EMAIL" type="email" value="" placeholder="Mon email" />
+							<div id="mce-responses" class="clear">
+								<div id="mce-error-response" class="response" style="display: none;"></div>
+								<div id="mce-success-response" class="response" style="display: none;"></div>
+							</div>
 						</div>
+						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input tabindex="-1" name="b_5bd0d33a577ee592df934d133_6ea9ff353e" type="text" value="" /></div>
+						<div class="clear"><input id="mc-embedded-subscribe" class="button" name="subscribe" type="submit" value="M'abonner" /></div>
 					</div>
-					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input tabindex="-1" name="b_5bd0d33a577ee592df934d133_6ea9ff353e" type="text" value="" /></div>
-					<div class="clear"><input id="mc-embedded-subscribe" class="button" name="subscribe" type="submit" value="S'abonner" /></div>
 				</div>
 			</div>
+		</form></div>
+		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email'; /*
+ 		* Translated default messages for the $ validation plugin.
+		 * Locale: FR
+		 */
+		 $.extend($.validator.messages, {
+		 	required: "Ce champ est requis.",
+		 	remote: "Veuillez remplir ce champ pour continuer.",
+		 	email: "Veuillez entrer une adresse email valide.",
+		 });}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+		 <!--End mc_embed_signup-->
 		</div>
-	</form></div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email'; /*
- * Translated default messages for the $ validation plugin.
- * Locale: FR
- */
- $.extend($.validator.messages, {
- 	required: "Ce champ est requis.",
- 	remote: "Veuillez remplir ce champ pour continuer.",
- 	email: "Veuillez entrer une adresse email valide.",
- });}(jQuery));var $mcj = jQuery.noConflict(true);</script>
- <!--End mc_embed_signup-->
-</div>
 
-<div class="row">
-	<div class="last-videos"></div>
-</div>
+		<div class="row">
+			<div class="last-videos col-xs-12 col-lg-6">
+				<ul>
+					<li><img src="http://localhost:8888/perso/les_flux/wordpress/wp-content/uploads/2018/05/cropped-lightpainting_chatte_web-768x481.jpg" alt="dernières vidéos" class="thumbnail-last-videos"></li>
+					<li><a href="#"><h4>Voir les dernières vidéos</h4></a></li>
+				</ul>
+			</div>
 
-<div class="row">
-	<div class="last-ateliers"></div>
-</div>
+			<div class="last-ateliers col-xs-12 col-lg-6">
+				<ul>
+					<li><img src="http://localhost:8888/perso/les_flux/wordpress/wp-content/uploads/2018/05/cropped-lightpainting_chatte_web-768x481.jpg" alt="prochains ateliers" class="thumbnail-last-ateliers"></li>
+					<li><a href="#"><h4>Voir les dates des prochains ateliers</h4></a></li>
+				</ul>
+			</div>
+		</div>
 
+	</div>
 </div>
 
 <?php
